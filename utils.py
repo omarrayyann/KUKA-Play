75e23dcfd532dbb59e3ba345de435c55b9ce816f
Omar <olr7742@nyu.edu>
2024-03-30 06:32:54 -0400

    Update

--- utils.py
+++ utils.py
@@ -1,3 +1,15 @@
+import numpy as np
+import time
+
+def move_joints_position(viz, q):
+    viz.display(np.array(q))
+
+def move_trajectory(viz, frequency, trajectory):
+    while not trajectory.done():
+        move_joints_position(viz, trajectory.current_q)
+        trajectory.step(1/frequency)
+        time.sleep(1/frequency)
+
 class Trajectory:
     
     def __init__(self, from_q, to_q, total_time):
